a119ad904a06689daaf5aa86ad9f3d28c2f21241
Sahana Tankala <sahanatankala@gmail.com>
2024-12-20 21:02:18 -0500

    initial flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake for running and building aSkier";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            zsh
+
+            # ocaml packages
+            ocamlPackages.ocaml
+            ocamlPackages.dune_3
+          ];
+        };
+      }
+    );
+}
