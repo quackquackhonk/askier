a8e7f6425e3e1a71800174cf15a6f81fd77aca3c
Sahana Tankala <sahanatankala@gmail.com>
2024-12-21 13:49:53 -0500

    formatters

--- flake.nix
+++ flake.nix
@@ -8,7 +8,12 @@
   };
 
   outputs =
-    { self, nixpkgs, flake-utils, ... }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
@@ -16,70 +21,74 @@
         pkgs = nixpkgs.legacyPackages.${system};
         ocamlPackages = pkgs.ocamlPackages;
       in
-        {
-          # Main askier package
-          # run with:
-          #     nix build
-          #     nix run -- args...
-          packages = {
-            default = self.packages.${system}.askier;
+      {
+        # Main askier package
+        # run with:
+        #     nix build
+        #     nix run -- args...
+        packages = {
+          default = self.packages.${system}.askier;
 
-            askier = ocamlPackages.buildDunePackage {
-              pname = "askier";
-              version = "0.1.0";
-              duneVersion = "3";
-              src = ./.;
+          askier = ocamlPackages.buildDunePackage {
+            pname = "askier";
+            version = "0.1.0";
+            duneVersion = "3";
+            src = ./.;
 
-              buildInputs = [
-                # OCaml package dependencies go here.
-              ];
+            buildInputs = [
+              # OCaml package dependencies go here.
+            ];
 
-              strictDeps = true;
+            strictDeps = true;
 
-            };
           };
+        };
 
-          # Flake checks
-          #
-          #     $ nix flake check
-          #
-          checks = {
-            # Run tests for the `hello` package
-            askier =
-              let
-                # Patches calls to dune commands to produce log-friendly output
-                # when using `nix ... --print-build-log`. Ideally there would be
-                # support for one or more of the following:
-                #
-                # In Dune:
-                #
-                # - have workspace-specific dune configuration files
-                #
-                # In NixPkgs:
-                #
-                # - allow dune flags to be set in in `ocamlPackages.buildDunePackage`
-                # - alter `ocamlPackages.buildDunePackage` to use `--display=short`
-                # - alter `ocamlPackages.buildDunePackage` to allow `--config-file=FILE` to be set
-                patchDuneCommand =
-                  let
-                    subcmds = [ "build" "test" "runtest" "install" ];
-                  in
-                    lib.replaceStrings
-                      (lib.lists.map (subcmd: "dune ${subcmd}") subcmds)
-                      (lib.lists.map (subcmd: "dune ${subcmd} --display=short") subcmds);
-              in
+        # Flake checks
+        #
+        #     $ nix flake check
+        #
+        checks = {
+          # Run tests for the `hello` package
+          askier =
+            let
+              # Patches calls to dune commands to produce log-friendly output
+              # when using `nix ... --print-build-log`. Ideally there would be
+              # support for one or more of the following:
+              #
+              # In Dune:
+              #
+              # - have workspace-specific dune configuration files
+              #
+              # In NixPkgs:
+              #
+              # - allow dune flags to be set in in `ocamlPackages.buildDunePackage`
+              # - alter `ocamlPackages.buildDunePackage` to use `--display=short`
+              # - alter `ocamlPackages.buildDunePackage` to allow `--config-file=FILE` to be set
+              patchDuneCommand =
+                let
+                  subcmds = [
+                    "build"
+                    "test"
+                    "runtest"
+                    "install"
+                  ];
+                in
+                lib.replaceStrings (lib.lists.map (subcmd: "dune ${subcmd}") subcmds) (
+                  lib.lists.map (subcmd: "dune ${subcmd} --display=short") subcmds
+                );
+            in
+            self.packages.${system}.askier.overrideAttrs (oldAttrs: {
+              name = "check-${oldAttrs.name}";
+              doCheck = true;
+              buildPhase = patchDuneCommand oldAttrs.buildPhase;
+              checkPhase = patchDuneCommand oldAttrs.checkPhase;
+              # installPhase = patchDuneCommand oldAttrs.checkPhase;
+            });
 
-                self.packages.${system}.hello.overrideAttrs
-                  (oldAttrs: {
-                    name = "check-${oldAttrs.name}";
-                    doCheck = true;
-                    buildPhase = patchDuneCommand oldAttrs.buildPhase;
-                    checkPhase = patchDuneCommand oldAttrs.checkPhase;
-                    # installPhase = patchDuneCommand oldAttrs.checkPhase;
-                  });
-
-            # Check Dune and OCaml formatting
-            dune-fmt = pkgs.runCommand "check-dune-fmt"
+          # Check Dune and OCaml formatting
+          dune-fmt =
+            pkgs.runCommand "check-dune-fmt"
               {
                 nativeBuildInputs = [
                   ocamlPackages.dune_3
@@ -88,18 +97,19 @@
                 ];
               }
               ''
-              echo "checking dune and ocaml formatting"
-              dune build \
-                --display=short \
-                --no-print-directory \
-                --root="${./.}" \
-                --build-dir="$(pwd)/_build" \
-                @fmt
-              touch $out
-            '';
+                echo "checking dune and ocaml formatting"
+                dune build \
+                  --display=short \
+                  --no-print-directory \
+                  --root="${./.}" \
+                  --build-dir="$(pwd)/_build" \
+                  @fmt
+                touch $out
+              '';
 
-            # Check documentation generation
-            dune-doc = pkgs.runCommand "check-dune-doc"
+          # Check documentation generation
+          dune-doc =
+            pkgs.runCommand "check-dune-doc"
               {
                 ODOC_WARN_ERROR = "true";
                 nativeBuildInputs = [
@@ -109,37 +119,29 @@
                 ];
               }
               ''
-              echo "checking ocaml documentation"
-              dune build \
-                --display=short \
-                --no-print-directory \
-                --root="${./.}" \
-                --build-dir="$(pwd)/_build" \
-                @doc
-              touch $out
-            '';
+                echo "checking ocaml documentation"
+                dune build \
+                  --display=short \
+                  --no-print-directory \
+                  --root="${./.}" \
+                  --build-dir="$(pwd)/_build" \
+                  @doc
+                touch $out
+              '';
+        };
 
-            # Check Nix formatting
-            nixpkgs-fmt = pkgs.runCommand "check-nixpkgs-fmt"
-              { nativeBuildInputs = [ pkgs.nixpkgs-fmt ]; }
-              ''
-              echo "checking nix formatting"
-              nixpkgs-fmt --check ${./.}
-              touch $out
-            '';
-          };
-
-          devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             zsh
 
             # ocaml packages
-            ocamlPackages.ocaml
-            ocamlPackages.dune_3
-            ocamlPackages.utop
-            ocamlPackages.findlib
+            ocamlPackages.ocaml # its ocaml
+            ocamlPackages.dune_3 # build system
+            ocamlPackages.utop # fancy fancy repl
+            ocamlPackages.findlib # necessary for nixos
+            ocamlPackages.merlin # pseudo-lsp
           ];
         };
-    }
-);
+      }
+    );
 }
